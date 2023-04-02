34163ac73944402b9fd14770a29281e37c57cf7f
programmerrmasud <programmerrmasud@gmail.com>
2023-04-02 12:41:52 +0600

    Auth

--- resources/views/BackEnd/User/Auth/register.blade.php
+++ resources/views/BackEnd/User/Auth/register.blade.php
@@ -1,5 +1,5 @@
 @extends('BackEnd.User.Layouts.app')
-@section('title','Login Page')
+@section('title','Register Page')
 @section('content')
 	<!-- Page container -->
 	<div class="page-container login-container">
@@ -18,11 +18,17 @@
 						<div class="panel panel-body login-form">
 							<div class="text-center">
 								<div class="icon-object border-slate-300 text-slate-300"><i class="icon-reading"></i></div>
-								<h5 class="content-group">Login to your account <small class="display-block">Enter your credentials below</small></h5>
+								<h5 class="content-group">Create a new account <small class="display-block">Enter your credentials below</small></h5>
 							</div>
 
 							<div class="form-group has-feedback has-feedback-left">
-								<input type="text" class="form-control" placeholder="Username">
+								<input type="text" class="form-control" placeholder="Your Name">
+								<div class="form-control-feedback">
+									<i class="icon-user text-muted"></i>
+								</div>
+							</div>
+							<div class="form-group has-feedback has-feedback-left">
+								<input type="text" class="form-control" placeholder="Your Email">
 								<div class="form-control-feedback">
 									<i class="icon-user text-muted"></i>
 								</div>
@@ -36,7 +42,8 @@
 							</div>
 
 							<div class="form-group">
-								<button type="submit" class="btn btn-primary btn-block">Sign in <i class="icon-circle-right2 position-right"></i></button>
+								<button type="submit" class="btn btn-primary btn-block">Sign Up<i class="icon-circle-right2 position-right"></i></button>
+								<button type="submit" class="btn btn-success btn-block"> Already Have an Account ? <i class="icon-circle-leftposition-left"></i></button>
 							</div>
 
 							<div class="text-center">
@@ -49,7 +56,7 @@
 
 					<!-- Footer -->
 					<div class="footer text-muted">
-						&copy; 2015. <a href="#">Learning Managment System</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">M.H. Aadeez</a>
+						&copy; 2023. <a href="#">Learning Managment System</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">M.H. Aadeez</a>
 					</div>
 					<!-- /footer -->
 
